07fcaddcc090e93580a5658803132fccac3e505b
Inge Intania <ingeintania@Inges-MacBook-Pro.local>
2025-09-07 22:49:00 +0700

    add json and make it better

--- README.md
+++ README.md
@@ -2,9 +2,30 @@
 
 Run with
 
-    mvn exec:java -Dexec.mainClass="net.sourceforge.pmd.examples.maven.simpleproject.Main"
+    mvn exec:java \
+  -Dexec.mainClass="net.sourceforge.pmd.examples.maven.simpleproject.Main" \
+  -Dexec.args="<repoPath> <rulesetPath>"
+  
+    example:
+    mvn exec:java \
+  -Dexec.mainClass="net.sourceforge.pmd.examples.maven.simpleproject.Main" \
+  -Dexec.args="/Users/ingeintania/Documents/code/commons-lang /Users/ingeintania/Documents/code/pmd-repo-miner/ruleset.xml"
 
-This should generate report.json, the format should be as follows:
+This should generate JSON on each commit commit_[index of commit].json, the format should be as follows:
+
+    {
+        "commit_hash" : "d6f6665f263c374e2d49c5e6e54c10dc91e5a398",
+        "commit_index" : 1,
+        "num_java_files" : 12,
+        "num_warnings" : 7,
+        "stat_of_warnings" : {
+            "UnusedPrivateField" : 4,
+            "SystemPrintln" : 3,
+            ...
+        }
+    }
+
+This also should generate summary.json, the format should be as follows:
 
     {
         "location" : "/a/b/c",
--- src/main/java/net/sourceforge/pmd/examples/maven/simpleproject/Main.java
+++ src/main/java/net/sourceforge/pmd/examples/maven/simpleproject/Main.java
@@ -1,8 +1,12 @@
 package net.sourceforge.pmd.examples.maven.simpleproject;
 
 import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.api.errors.CheckoutConflictException;
 import org.eclipse.jgit.api.errors.GitAPIException;
+import org.eclipse.jgit.api.errors.InvalidRefNameException;
 import org.eclipse.jgit.api.errors.NoHeadException;
+import org.eclipse.jgit.api.errors.RefAlreadyExistsException;
+import org.eclipse.jgit.api.errors.RefNotFoundException;
 import org.eclipse.jgit.revwalk.RevCommit;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -20,58 +24,18 @@ import java.util.Map;
 
 public class Main {
     public static void main(String[] args) throws IOException, NoHeadException, GitAPIException {
-        //Change your target path on repoPath
-        String repoPath = "/Users/ingeintania/Documents/code/commons-lang";
+        // Change your target path on repoPath
+        if (args.length < 2) {
+            System.out.println("Please input <repoPath> <rulesetPath>");
+            return;
+        }
+
+        String repoPath = args[0]; // repoPath input
+        String rulesetPath = args[1]; // rulesetPath input
         File repoDir = new File(repoPath);
 
-        int commits = 0, javaFiles = 0;
-        try (Git git = Git.open(repoDir)) {
-            for (RevCommit c : git.log().call()) {
-                commits++;
-                javaFiles += countJavaFiles(repoDir);
-            }
-        }
-
-        // Create JSON
-        ObjectMapper mapper = new ObjectMapper();
-        ObjectNode root = mapper.createObjectNode();
-        root.put("location", repoDir.getAbsolutePath());
-
-        ObjectNode statRepo = root.putObject("stat_of_repository");
-        statRepo.put("number_of_commits", commits);
-
-        Double avg_of_num_java_files = (double) javaFiles / commits;
-        statRepo.put("avg_of_num_java_files", Math.round(avg_of_num_java_files * 10) / 10.0);
-
-        // Call warnings with PMD
-        try {
-            PMDConfiguration cfg = new PMDConfiguration();
-            cfg.addInputPath(repoDir.toPath());
-            cfg.addRuleSet("ruleset.xml");
-
-            try (PmdAnalysis pmd = PmdAnalysis.create(cfg)) {
-                Report report = pmd.performAnalysisAndCollectReport();
-
-                Map<String, Integer> allRules = new HashMap<>();
-
-                for (RuleViolation v : report.getViolations()) {
-                    String r = v.getRule().getName();
-                    allRules.put(r, allRules.getOrDefault(r, 0)+1);
-                }
-                Double avg_of_num_warnings = (double) report.getViolations().size() / commits;
-                statRepo.put("avg_of_num_warnings", Math.round(avg_of_num_warnings * 10) / 10.0);
-                ObjectNode statWarnings = root.putObject("stat_of_warnings");
-                for (Map.Entry<String, Integer> entry : allRules.entrySet()) {
-                    statWarnings.put(entry.getKey(), entry.getValue());
-                }
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-        // Save to JSON file
-        mapper.writerWithDefaultPrettyPrinter().writeValue(new File("report.json"), root);
-        System.out.println("JSON Report generated: report.json");
+        jsonEachCommit(repoDir, rulesetPath);
+        jsonSummary(repoDir, rulesetPath);
     }
 
     private static int countJavaFiles(File dir) {
@@ -87,5 +51,119 @@ public class Main {
             }
         }
         return count;
+    }
+
+    public static void jsonEachCommit(File repoDir, String rulesetPath)
+            throws IOException, NoHeadException, RefAlreadyExistsException, RefNotFoundException,
+            InvalidRefNameException, CheckoutConflictException, GitAPIException {
+        int commitIndex = 0;
+
+        Git git = Git.open(repoDir);
+        for (RevCommit c : git.log().all().call()) {
+            // Checkout tiap commit
+            git.reset().setMode(org.eclipse.jgit.api.ResetCommand.ResetType.HARD).call();
+            git.clean().setCleanDirectories(true).setForce(true).call();
+            git.checkout().setName(c.getName()).setForced(true).call();
+            commitIndex++;
+
+            // Hitung Java files di commit ini
+            int javaFiles = countJavaFiles(repoDir);
+
+            // Run PMD di commit ini
+            PMDConfiguration cfg = new PMDConfiguration();
+            cfg.addInputPath(repoDir.toPath());
+            cfg.addRuleSet(rulesetPath);
+
+            Map<String, Integer> commitRules = new HashMap<>();
+            int numWarnings = 0;
+
+            try (PmdAnalysis pmd = PmdAnalysis.create(cfg)) {
+                Report report = pmd.performAnalysisAndCollectReport();
+                numWarnings = report.getViolations().size();
+
+                for (RuleViolation v : report.getViolations()) {
+                    String r = v.getRule().getName();
+                    commitRules.put(r, commitRules.getOrDefault(r, 0) + 1);
+                }
+            }
+
+            // Create JSON per commit
+            ObjectMapper mapper = new ObjectMapper();
+            ObjectNode rootCommit = mapper.createObjectNode();
+            rootCommit.put("commit_hash", c.getName());
+            rootCommit.put("commit_index", commitIndex);
+            rootCommit.put("num_java_files", javaFiles);
+            rootCommit.put("num_warnings", numWarnings);
+
+            ObjectNode statWarnings = rootCommit.putObject("stat_of_warnings");
+            for (Map.Entry<String, Integer> entry : commitRules.entrySet()) {
+                statWarnings.put(entry.getKey(), entry.getValue());
+            }
+
+            // Simpan file JSON
+            File outFile = new File("reports/commits/commit_" + commitIndex + ".json");
+            outFile.getParentFile().mkdirs(); // bikin folder kalau belum ada
+            mapper.writerWithDefaultPrettyPrinter().writeValue(outFile, rootCommit);
+
+            System.out.println("Generated JSON for commit " + commitIndex + ": " + outFile.getPath());
+        }
+    }
+
+    public static void jsonSummary(File repoDir, String rulesetPath)
+            throws IOException, NoHeadException, GitAPIException {
+        // Call warnings with PMD
+        int commits = 0;
+        int javaFiles = 0;
+        int totalJavaFiles = 0;
+        int totalWarnings = 0;
+        Map<String, Integer> allRules = new HashMap<>();
+
+        try (Git git = Git.open(repoDir)) {
+            for (RevCommit c : git.log().all().call()) {
+                git.reset().setMode(org.eclipse.jgit.api.ResetCommand.ResetType.HARD).call();
+                git.clean().setCleanDirectories(true).setForce(true).call();
+                git.checkout().setName(c.getName()).setForced(true).call();
+                commits++;
+
+                javaFiles = countJavaFiles(repoDir);
+                totalJavaFiles += javaFiles;
+
+                PMDConfiguration cfg = new PMDConfiguration();
+                cfg.addInputPath(repoDir.toPath());
+                cfg.addRuleSet(rulesetPath);
+
+                try (PmdAnalysis pmd = PmdAnalysis.create(cfg)) {
+                    Report report = pmd.performAnalysisAndCollectReport();
+                    totalWarnings += report.getViolations().size();
+
+                    for (RuleViolation v : report.getViolations()) {
+                        String r = v.getRule().getName();
+                        allRules.put(r, allRules.getOrDefault(r, 0) + 1);
+                    }
+                }
+            }
+        }
+
+        // Create JSON
+        ObjectMapper mapper = new ObjectMapper();
+        ObjectNode root = mapper.createObjectNode();
+        root.put("location", repoDir.getAbsolutePath());
+
+        ObjectNode statRepo = root.putObject("stat_of_repository");
+        statRepo.put("number_of_commits", commits);
+
+        Double avg_of_num_java_files = (double) totalJavaFiles / commits;
+        statRepo.put("avg_of_num_java_files", Math.round(avg_of_num_java_files * 10) / 10.0);
+
+        Double avg_of_num_warnings = (double) totalWarnings / commits;
+        statRepo.put("avg_of_num_warnings", Math.round(avg_of_num_warnings * 10) / 10.0);
+        ObjectNode statWarnings = root.putObject("stat_of_warnings");
+        for (Map.Entry<String, Integer> entry : allRules.entrySet()) {
+            statWarnings.put(entry.getKey(), entry.getValue());
+        }
+
+        // Save JSON Summary
+        mapper.writerWithDefaultPrettyPrinter().writeValue(new File("reports/summary.json"), root);
+        System.out.println("JSON Report generated: summary.json");
     }
 }
